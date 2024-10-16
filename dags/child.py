7d7c5a7b271729f27f39d4972fb471009d8d4bae
Mason Maeder <mjmaeder@wisc.edu>
2024-10-16 16:02:29 -0500

    Pause project

--- dags/child.py
+++ dags/child.py
@@ -1,7 +1,7 @@
-from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from airflow.sensors.external_task import ExternalTaskSensor
-from airflow.utils.dates import days_ago
+from airflow import DAG  # type: ignore
+from airflow.operators.dummy import DummyOperator  # type: ignore
+from airflow.sensors.external_task import ExternalTaskSensor  # type: ignore
+from airflow.utils.dates import days_ago  # type: ignore
 
 default_args = {
     'owner': 'airflow',
@@ -12,25 +12,21 @@ default_args = {
 with DAG(
     dag_id='child',
     default_args=default_args,
-    description='A child DAG that runs only when the parent DAG succeeds.',
+    description='child dag',
     schedule_interval='@daily',
     start_date=days_ago(1),
     catchup=False,
 ) as dag:
-
-    # Sensor to wait for the parent DAG to succeed
     wait_for_parent_success = ExternalTaskSensor(
         task_id='wait_for_parent_success',
-        external_dag_id='parent_success',  # The ID of the successful parent DAG
-        external_task_id='end',  # The final task in the parent DAG
-        mode='poke',  # Keep checking periodically
-        timeout=600,  # Fail if not complete in 10 minutes
-        poke_interval=30,  # Check every 30 seconds
-        soft_fail=False,  # Hard fail if parent task does not succeed
+        external_dag_id='parent_success',
+        external_task_id='end',
+        mode='poke',  # check
+        timeout=600,  # 10 minutes
+        poke_interval=1,  # check every second
+        soft_fail=True,
     )
 
-    # Dummy task to indicate successful execution of the child DAG
     child_task = DummyOperator(task_id='child_task')
 
-    # Set dependencies
     wait_for_parent_success >> child_task
--- dags/parent_fail.py
+++ dags/parent_fail.py
@@ -1,6 +1,12 @@
 from airflow import DAG  # type: ignore
 from airflow.operators.dummy import DummyOperator  # type: ignore
+from airflow.operators.python import PythonOperator  # type: ignore
 from airflow.utils.dates import days_ago  # type: ignore
+
+
+def fail_task():
+    raise Exception("intentional failure")
+
 
 default_args = {
     'owner': 'airflow',
@@ -9,15 +15,14 @@ default_args = {
 }
 
 with DAG(
-    dag_id='parent_success',
+    dag_id='parent_fail',
     default_args=default_args,
-    description='parent succeeds',
+    description='parent fails',
     schedule_interval='@daily',
     start_date=days_ago(1),
     catchup=False,
 ) as dag:
-
     start = DummyOperator(task_id='start')
-    end = DummyOperator(task_id='end')
+    fail = PythonOperator(task_id='fail', python_callable=fail_task)
 
-    start >> end
+    start >> fail
